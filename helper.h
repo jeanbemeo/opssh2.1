aaa7068aff7071905dc9a6c8ad3ef49dc3742efb
jeanbemeo <jeanbe504@gmail.com>
2025-01-28 08:33:30 -0800

    Update helper.h

--- helper.h
+++ helper.h
@@ -1,18 +1,8 @@
 #ifndef HELPER_H
 #define HELPER_H
 
-#include <stdint.h>
-#include <stdlib.h>
-
-// Function declarations
-int setup_connection(const char *ip, int port);
-void send_packet(int sock, unsigned char packet_type, const unsigned char *data, size_t len);
-void prepare_heap(int sock);
-void create_fake_file_structure(unsigned char *data, size_t size, uint64_t glibc_base);
-void time_final_packet(int sock, double *parsing_time);
-double measure_response_time(int sock, int error_type);
-
-extern uint64_t GLIBC_BASES[];
-extern int NUM_GLIBC_BASES;
+int send_packet(int sock, const char *data, size_t len);
+int perform_ssh_handshake(int sock);
+int attempt_race_condition(int sock, double parsing_time, unsigned long glibc_base);
 
 #endif
